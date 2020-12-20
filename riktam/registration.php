ad29ee1030657f24b967aba3fdd56d7729ff906d
vanitha223 <40634017+vanitha223@users.noreply.github.com>
2020-12-20 23:43:31 +0530

    Add files via upload

---
+++ riktam/registration.php
@@ -0,0 +1,152 @@
+<?php
+$con = mysqli_connect("localhost","root","","smart_chair");
+
+if(isset($_POST['submit']))
+{
+$fname=$_POST['full_name'];
+$address=$_POST['address'];
+$city=$_POST['city'];
+$gender=$_POST['gender'];
+$email=$_POST['email'];
+$password=md5($_POST['password']);
+$query=mysqli_query($con,"insert into users(fullname,address,city,gender,email,password) values('$fname','$address','$city','$gender','$email','$password')");
+if($query)
+{
+	echo "<script>alert('Successfully Registered. You can login now');</script>";
+	header('location:user-login.php');
+}
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<title>User Registration</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta content="" name="description" />
+		<meta content="" name="author" />
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css"><!--to Align reg fields in proper way-->
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css"><!--Email Password Icons-->
+		<link rel="stylesheet" href="assets/css/styles.css"><!--to assign blue color and arrage icons-->
+		<script type="text/javascript">
+function valid()
+{
+var a=document.getElementById("fullname").value
+var b= document.getElementById("password").value
+var reu=/\W/
+if(reu.test(a))
+{
+alert("Username should not contain spaces / Special symbols")
+return false;
+}
+else if(b.length<8)
+{
+alert("Password must contains minimum 8 characters")
+return false
+}
+ else if(document.registration.password.value!= document.registration.password_again.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.registration.password_again.focus();
+return false;
+}
+else{
+return true;
+}
+}
+</script>
+		
+
+	</head>
+
+	<body class="login">
+	<div class="row">
+			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+				<div class="logo margin-top-30">
+				<h2>Smart Chair | User Registration</h2>
+				</div>
+				<!-- start: REGISTER BOX -->
+				<div class="box-register">
+					<form name="registration" id="registration"  method="post" onSubmit="return valid();">
+						<fieldset>
+							<legend>
+								Sign Up
+							</legend>
+							<p>
+								Enter your personal details below:
+							</p>
+							<div class="form-group">
+								<input type="text" class="form-control" name="full_name" placeholder="Full Name" id="fullname" required>
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" name="address" placeholder="Address" required>
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" name="city" placeholder="City" required>
+							</div>
+							<div class="form-group">
+								<label class="block">
+									Gender
+								</label>
+								<div>
+									<input type="radio" id="rg-female" name="gender" value="female" >
+										Female
+									<input type="radio" id="rg-male" name="gender" value="male">
+										Male
+								</div>
+							</div>
+							<p>
+								Enter your account details below:
+							</p>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="email" class="form-control" name="email" id="email" onBlur="userAvailability()"  placeholder="Email" required>
+									<i class="fa fa-envelope"></i> </span>
+									 <span id="user-availability-status1" style="font-size:12px;"></span>
+							</div>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+									<i class="fa fa-lock"></i> </span>
+							</div>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="password" class="form-control"  id="password_again" name="password_again" placeholder="Password Again" required>
+									<i class="fa fa-lock"></i> </span>
+							</div>
+							<div class="form-group">
+								<div class="checkbox clip-check check-primary">
+									<input type="checkbox" id="agree" value="agree" readonly=" true">
+									<label for="agree">
+										I agree
+									</label>
+								</div>
+							</div>
+							<div class="form-actions">
+								<p>
+									Already have an account?
+									<a href="user-login.php">
+										Log-in
+									</a>
+								</p>
+								<button type="submit" class="btn btn-primary pull-right" id="submit" name="submit">
+									Submit <i class="fa fa-arrow-circle-right"></i>
+								</button>
+							</div>
+						</fieldset>
+					</form>
+				</div>
+			</div>
+		</div>
+		
+	<script>
+
+</script>			
+</body>
+</html>
---
+++ riktam/upload_issue.php
@@ -0,0 +1,237 @@
+<?php
+session_start();
+//error_reporting(0);
+include('include/config.php');
+include('include/checklogin.php');
+check_login();
+
+if(isset($_POST['submit']))
+{
+$patname=$_POST['patname'];
+$gender=$_POST['gender'];
+$age=$_POST['age'];
+$patcondition=$_POST['patcondition'];
+$patemail=$_POST['patemail'];
+$password=md5($_POST['npass']);
+$sql=mysqli_query($con,"insert into patients(patname,gender,age,patcondition,patemail,password) values('$patname','$gender','$age','$patcondition','$patemail','$password')");
+if($sql)
+{
+echo "<script>alert('Patient info added Successfully');</script>";
+header('location:manage-patients.php');
+
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Admin | Add Doctor</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta content="" name="description" />
+		<meta content="" name="author" />
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+<script type="text/javascript">
+function valid()
+{
+ if(document.adddoc.npass.value!= document.adddoc.cfpass.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.adddoc.cfpass.focus();
+return false;
+}
+return true;
+}
+</script>
+
+
+<script>
+function checkemailAvailability() {
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'emailid='+$("#docemail").val(),
+type: "POST",
+success:function(data){
+$("#email-availability-status").html(data);
+$("#loaderIcon").hide();
+},
+error:function (){}
+});
+}
+</script>
+	</head>
+	<body>
+		<div id="app">		
+<?php include('include/sidebar.php');?>
+			<div class="app-content">
+				
+						<?php include('include/header.php');?>
+						
+				<!-- end: TOP NAVBAR -->
+				<div class="main-content" >
+					<div class="wrap-content container" id="container">
+						<!-- start: PAGE TITLE -->
+						<section id="page-title">
+							<div class="row">
+								<div class="col-sm-8">
+									<h1 class="mainTitle">User | Add Issue</h1>
+																	</div>
+								<ol class="breadcrumb">
+									<li>
+										<span>User</span>
+									</li>
+									<li class="active">
+										<span>Add Issue</span>
+									</li>
+								</ol>
+							</div>
+						</section>
+						<!-- end: PAGE TITLE -->
+						<!-- start: BASIC EXAMPLE -->
+						<div class="container-fluid container-fullw bg-white">
+							<div class="row">
+								<div class="col-md-12">
+									
+									<div class="row margin-top-30">
+										<div class="col-lg-8 col-md-12">
+											<div class="panel panel-white">
+												<div class="panel-heading">
+													<h5 class="panel-title">Add Issue</h5>
+												</div>
+												<div class="panel-body">
+									
+													<form role="form" name="adddoc" method="post" onSubmit="return valid();">
+
+<div class="form-group">
+															<label for="patientname">
+																 Location
+															</label>
+					<input type="text" name="patname" class="form-control"  placeholder="Enter Location of the Issue required="true">
+														</div>
+										
+								
+
+
+<div class="form-group">
+															<label for="Date">
+																Date
+															</label>
+					<input type="date" name="patcondition" class="form-control"  placeholder="write some words about patient" required="true"></textarea>
+														</div>
+<div class="form-group">
+									<label for="fess">
+																Time
+															</label>
+<input type="time" id="docemail" name="patemail" class="form-control"  placeholder="Enter Patient Email id" required="true" onBlur="checkemailAvailability()">
+<span id="email-availability-status"></span>
+</div>
+										
+                <div class="form-group">
+															<label for="exampleInputPassword2">
+																Upload Image
+															</label>
+									<input type="file" name="upload" class="form-control"  accept = "image/*" required="required">
+														</div>
+
+
+
+														
+														<div class="form-group">
+															<label for="exampleInputPassword1">
+																 Description
+															</label>
+					<input type="text" name="npass" class="form-control"  placeholder="Describe the Issue" required="required">
+														</div>
+				
+														
+														
+														
+														<button type="submit" name="submit" id="submit" class="btn btn-o btn-primary">
+															Submit
+														</button>
+													</form>
+												</div>
+											</div>
+										</div>
+											
+											</div>
+										</div>
+									<div class="col-lg-12 col-md-12">
+											<div class="panel panel-white">
+												
+												
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- end: BASIC EXAMPLE -->
+			
+					
+					
+						
+						
+					
+						<!-- end: SELECT BOXES -->
+						
+					</div>
+				</div>
+			</div>
+			<!-- start: FOOTER -->
+	<?php include('include/footer.php');?>
+			<!-- end: FOOTER -->
+		
+			<!-- start: SETTINGS -->
+	<?php include('include/setting.php');?>
+			<>
+			<!-- end: SETTINGS -->
+		</div>
+		<!-- start: MAIN JAVASCRIPTS -->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/modernizr/modernizr.js"></script>
+		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="vendor/switchery/switchery.min.js"></script>
+		<!-- end: MAIN JAVASCRIPTS -->
+		<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
+		<script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+		<script src="vendor/autosize/autosize.min.js"></script>
+		<script src="vendor/selectFx/classie.js"></script>
+		<script src="vendor/selectFx/selectFx.js"></script>
+		<script src="vendor/select2/select2.min.js"></script>
+		<script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+		<script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<!-- start: CLIP-TWO JAVASCRIPTS -->
+		<script src="assets/js/main.js"></script>
+		<!-- start: JavaScript Event Handlers for this page -->
+		<script src="assets/js/form-elements.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				FormElements.init();
+			});
+		</script>
+		<!-- end: JavaScript Event Handlers for this page -->
+		<!-- end: CLIP-TWO JAVASCRIPTS -->
+	</body>
+</html>
