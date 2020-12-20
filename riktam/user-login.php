79cd5d591d33931b5097c3de1d8db13cbffdb471
vanitha223 <40634017+vanitha223@users.noreply.github.com>
2020-12-20 23:53:24 +0530

    Update user-login.php

--- riktam/user-login.php
+++ riktam/user-login.php
@@ -63,7 +63,7 @@ exit();
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<a href="../index.html"><h2> Smart Chair | User Login</h2></a>
+				<a href="../index.html"><h2> Civic Issues | User Login</h2></a>
 				</div>
 
 				<div class="box-login">
