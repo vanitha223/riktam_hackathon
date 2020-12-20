c3e4f2fcb3b5826c54b97f462e4859d2edc50087
vanitha223 <40634017+vanitha223@users.noreply.github.com>
2020-12-20 23:52:49 +0530

    Update registration.php

--- riktam/registration.php
+++ riktam/registration.php
@@ -69,7 +69,7 @@ return true;
 	<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<h2>Smart Chair | User Registration</h2>
+				<h2>Civic Issue | User Registration</h2>
 				</div>
 				<!-- start: REGISTER BOX -->
 				<div class="box-register">
